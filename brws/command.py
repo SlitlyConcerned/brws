3859ee33ef21b9c7ccb7ac9a35ed74532fb7cb45
SlitlyConcerned <slitly@zohomail.coom>
2019-12-17 07:59:13 +0900

    Add command.py

---
+++ brws/command.py
@@ -0,0 +1,12 @@
+import doc
+
+
+def print_command(command):
+    command_name, function = command
+    print(command_name ": ", end="")
+    doc.print_function(function, if_none="No Documentation")
+
+
+def print_commands(commands):
+    for command in commands:
+        print(command)
